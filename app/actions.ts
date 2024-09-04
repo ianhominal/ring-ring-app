8a785110df4d20c515a1e8ee1e207204ce211b10
Ian Hominal <ianhominal@gmail.com>
2024-09-04 00:46:16 -0300

    change to telegram bot

--- app/actions.ts
+++ app/actions.ts
@@ -1,27 +1,25 @@
 "use server";
 
 export const callRing = async (name: string) => {
-    const response = await fetch('https://maker.ifttt.com/trigger/ring_pressed/json/with/key/5uEKdf7yEC2kwboMDNe4y', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ 
-            nombre: name
-         }),
-      });
-  
-      if(!response.ok) {
-        return null
-      }
-      const contentType = response.headers.get('content-type');
-      let data;
-  
-      if (contentType && contentType.includes('application/json')) {
-        data = await response.json(); // Parsear como JSON
-      } else {
-        data = await response.text(); // Parsear como texto
-      }
+  const chatId = process.env.NEXT_PUBLIC_CHAT_ID;  // Lee el chat_id de las variables de entorno
+  const botToken = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;  // Lee el token de bot de las variables de entorno
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
-      return data;
-} 
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: `Alguien ha tocado el timbre: ${name}`,
+    }),
+  });
+
+  if (!response.ok) {
+    return null;
+  }
+
+  const data = await response.json();  // Asume que siempre será JSON
+  return data;
+}
